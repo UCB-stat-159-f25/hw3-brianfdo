6ae7b79eeb0da63ef7b30c1b54e17d10498044f8
Brian Fernando <bmfernan@berkeley.edu>
2025-10-30 00:34:38 +0000

    added toml file and updated __init__.py file for ligotools

--- ligotools/__init__.py
+++ ligotools/__init__.py
@@ -1,1 +1,12 @@
+"""
+ligotools package
+
+This package provides tools for reading LIGO data
+files.  Data along with supporting documentation
+can be downloaded from the losc web site:
+https://losc.ligo.org
+"""
+
+__version__ = "0.0.1"
+
 from .readligo import *
